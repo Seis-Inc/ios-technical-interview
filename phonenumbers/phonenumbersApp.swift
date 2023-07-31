a899d4a4dd51f9ef7e72057ae95bb39384a315c4
Matt McMurry <matt@seis.com>
2023-07-31 11:22:36 -0600

    Initial Commit

---
+++ phonenumbers/phonenumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  phonenumbersApp.swift
+//  phonenumbers
+//
+//  Created by Matt McMurry on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct phonenumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
